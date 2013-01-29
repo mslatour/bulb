71bc6a69a6cb392e1ad7399d48370ba2250b4236
Moos Hueting <mooshueting@gmail.com>
2013-01-29 23:39:37 +0100

    First version of REST API:
    1) GET idea by ID
    2) DELETE idea by ID
    3) POST idea
    4) GET neighbours of idea by ID

--- bulb/urls.py
+++ bulb/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-from bulb.views import IdeaAPIView
+from bulb.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('bulb.views',
-  url(r'^idea/(?P<ideaId>\d+)/$', IdeaAPIView.as_view(), name='idea')
+  url(r'^idea/(?P<ideaId>\d+)/$', IdeaAPIView.as_view(), name='IdeaAPIView'),
+  url(r'^idea/(?P<ideaId>\d+)/neighbours/$', NeighbourAPIView.as_view(), name='NeighbourAPIView'),
+  url(r'^idea/$', IdeaCollectionAPIView.as_view(), name='idea')
   # Examples:
   # url(r'^$', 'bulb.views.home', name='home'),
   # url(r'^bulb/', include('bulb.foo.urls')),
--- bulb/views.py
+++ bulb/views.py
@@ -1,8 +1,63 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import requests, json
+
+N4J = "http://localhost:7474/db/data/cypher"
 
 class IdeaAPIView(APIView):
   
-  def get(self, request, ideaId):
-    return Response()
+  def get(self, request, ideaId=0, format=None):
+    query = "start x=node({ideaId}) return x"
+    params = {"ideaId": int(ideaId)}
+    headers = {'content-type': 'application/json'}
+
+    r = requests.post(N4J, data=json.dumps({"query": query, "params": params}), headers=headers)
+
+    return Response(r.json())
 #    {"id":ideaId,"name":"Idea"+str(ideaId)})
+
+  def delete(self, request, ideaId=None, format=None):
+    if ideaId:
+      query = "start n=node({ideaId}) delete n;"
+      params = {"ideaId": int(ideaId)}
+
+      headers = {'content-type': 'application/json'}
+
+      r = requests.post(N4J, data=json.dumps({"query": query, "params": params}), headers = headers)
+
+      return Response(r.json())
+
+class IdeaCollectionAPIView(APIView):
+  def post(self, request, format=None):
+    query = "create (n {title: {title}}) return ID(n);"
+    params = {"title": request.DATA['title']}
+
+    headers = {'content-type': 'application/json'}
+
+    r = requests.post(N4J, data=json.dumps({"query": query, "params": params}), headers = headers)
+
+    return Response(r.json())
+
+class NeighbourAPIView(APIView):
+  def get(self, request, ideaId=None, format=None):
+    if ideaId:
+      query = "start n=node({ideaId}) match n-[:IS_RELATED_TO]-m return m;"
+      params = {"ideaId": int(ideaId)}
+
+      headers = {'content-type': 'application/json'}
+
+      r = requests.post(N4J, data=json.dumps({"query": query, "params": params}), headers = headers)
+
+      return Response(r.json())
+
+  def post(self, request, ideaId=None, format=None):
+    if ideaId:
+      query = "start node1=node({ideaId}), node2=node({neighbourId}) create node1-[:IS_RELATED_TO]->node2;"
+      params = {"ideaId": int(ideaId), "neighbourId": request.DATA['neighbour']}
+
+      headers = {'content-type': 'application/json'}
+
+      r = requests.post(N4J, data=json.dumps({"query": query, "params": params}), headers = headers)
+
+      return Response(r.json())
+
