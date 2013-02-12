0edc20218eb109013d97bd5e6117018080127f38
Moos Hueting <mooshueting@gmail.com>
2013-02-12 21:31:57 +0100

    Added API call for list of ideas; removed Neo4J specific info from output.

--- bulb/views.py
+++ bulb/views.py
@@ -4,16 +4,33 @@ import requests, json
 
 N4J = "http://localhost:7474/db/data/cypher"
 
+def n4j2bulb(r):
+    # JSON to Python dict
+    j = r.json()
+    
+    # Returned list
+    output = []
+
+    for datapoint in j['data']:
+        # Returned dict
+        d = {}
+        for i, c in enumerate(j['columns']):
+            d[c] = datapoint[i]
+        output.append(d)
+
+    return output
+
+
 class IdeaAPIView(APIView):
   
   def get(self, request, ideaId=0, format=None):
-    query = "start x=node({ideaId}) return x"
+    query = "start x=node({ideaId}) return x.title as title, ID(x) as id"
     params = {"ideaId": int(ideaId)}
     headers = {'content-type': 'application/json'}
 
     r = requests.post(N4J, data=json.dumps({"query": query, "params": params}), headers=headers)
 
-    return Response(r.json())
+    return Response(n4j2bulb(r))
 #    {"id":ideaId,"name":"Idea"+str(ideaId)})
 
   def delete(self, request, ideaId=None, format=None):
@@ -28,6 +45,14 @@ class IdeaAPIView(APIView):
       return Response(r.json())
 
 class IdeaCollectionAPIView(APIView):
+  def get(self, request, format=None):
+    query = "start x=node(*) where ID(x) <> 0 return x.title as title, ID(x) as id"
+    headers = {'content-type': 'application/json'}
+
+    r = requests.post(N4J, data=json.dumps({"query": query}), headers=headers)
+
+    return Response(n4j2bulb(r))
+
   def post(self, request, format=None):
     query = "create (n {title: {title}}) return ID(n);"
     params = {"title": request.DATA['title']}
