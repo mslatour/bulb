2788aa4b8fb847c424f13581dc114ea8cd8e12f0
Moos Hueting <mooshueting@gmail.com>
2013-02-26 18:03:22 +0100

    Added functionality of showing neighbours in Web Interface by clicking on nodes.

--- static/js/all.js
+++ static/js/all.js
@@ -12,7 +12,11 @@ $(function() {
         // necessary for Django as Backbone does not add trailing slash
         url: function() {
             var origUrl = Backbone.Model.prototype.url.call(this);
-            return origUrl += origUrl.endsWith('/') ? '' : '/';
+
+            if (origUrl.substr(origUrl.length - 1) !== "/")
+                origUrl += '/';
+                
+            return origUrl;
         },
 
 
@@ -24,6 +28,7 @@ $(function() {
             if (!this.get("title")) {
                 this.set({"title": this.defaults().title});
             }
+            this.selected = false;
         },
 
     });
@@ -34,6 +39,7 @@ $(function() {
         template: _.template($('script#ideaRowTpl').html()),
 
         events: {
+            "click": "toggleSelected",
             "click button#id_delete": "remove",
             "click .verify button.yes": "removeVerifyTrue",
             "click .verify button.no": "removeVerifyFalse"
@@ -59,8 +65,82 @@ $(function() {
             this.render();
         },
 
+        toggleSelected: function(event) {
+            model = this.model;
+
+            // switch 'selected' flag
+            if (model.selected) {
+                model.selected = false;
+            } else {
+                // deselect all other models
+                model.collection.each(function(idea) {
+                    idea.selected = false;
+                });
+                model.selected = true;
+                // get neighbours
+                this.getNeighbours();
+            }
+
+        },
+
+        getNeighbours: function() {
+            model = this.model;
+            // deselect all other neighbours
+            model.collection.each(function(idea) {
+                idea.neighbour = false;
+            });
+            // get neighbours
+            r = $.ajax({
+                url: 'idea/' + this.model.id + '/neighbours/',
+                type: 'GET',
+                dataType: 'json',
+                success: function(result) {
+                    $.each(result, function(index, value) {
+                        neighbour = model.collection.where(value)[0]
+                        neighbour.neighbour = true;
+                    });
+                    // trigger change event on collection to rerender view
+                    model.collection.trigger('change');
+                }
+            });
+        },
+
         render: function() {
+            self = this; // we need to reference the model from the inner scope of functions
             $(this.el).html(this.template(this.model.toJSON()));
+
+            // make row drag- and droppable
+            $(this.el).draggable({opacity: 0.5, distance: 20, helper: "clone"});
+            $(this.el).droppable({
+                hoverClass: "info",
+                drop: function(event, ui) {
+                    startNode = ui.draggable;
+                    endNode = $(this);
+
+                    startNodeID = startNode.find('td.idea-id').html();
+                    endNodeID = endNode.find('td.idea-id').html();
+
+                    $.ajax({
+                        url: 'idea/' + startNodeID + '/neighbours/',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {"neighbour": endNodeID * 1}, // convert from string to int, quite ugly
+                        success: function(result) {
+                            startNode.find('td .linked').html('<strong>Linked!</strong>').delay(500).animate({opacity: 0.0}, 800);
+                            endNode.find('td .linked').html('<strong>Linked!</strong>').delay(500).animate({opacity: 0.0}, 800);
+                            startNode.getNeighbours();
+                        },
+                    });
+                },
+            });
+
+            // show selection by marking in green
+            if (this.model.selected)
+                $(this.el).addClass('success');
+
+            if (this.model.neighbour)
+                $(this.el).addClass('warning');
+
             return this;
         }   
 
@@ -104,6 +184,7 @@ $(function() {
             this.$el.html('');
             this.ideas.each(this.addOne, this);
             return this;
+
         }
     });
 
@@ -147,5 +228,7 @@ $(function() {
 
     var app = new AppView();
     app.render();
+
+    /* */
 
 });
