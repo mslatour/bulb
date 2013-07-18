825da0d7cf903a8937619152c27d5884ab45c78b
mslatour <sanderlator@gmail.com>
2013-07-18 22:18:38 +0200

    Fixed reference to old variable

--- adduser.sh
+++ adduser.sh
@@ -9,5 +9,5 @@ sha224=`python -c "import hashlib;print hashlib.sha224('$password').hexdigest()"
 idUserJson=`curl -s -X POST -H "Content-Type: application/json" -d "{\"query\":
 \"CREATE (user { username: '$username', password: '$sha224' } ) RETURN ID(user)\"}" $NEO4J_URL/db/data/cypher`
 idUser=`python -c "raw=$idUserJson;print raw['data'][0][0]"`
-curl -s -X POST -H "Content-Type: application/json" -d "{\"key\": \"username\", \"value\": \"$username\", \"uri\": \"node/$idUser\"}" $n4j/db/data/index/node/users
+curl -s -X POST -H "Content-Type: application/json" -d "{\"key\": \"username\",\"value\": \"$username\", \"uri\": \"node/$idUser\"}" $NEO4J_URL/db/data/index/node/users
 echo "Added user $username at node $idUser"
