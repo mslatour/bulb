0aae57a9c5e40daac9906eb3a15340098cf5c502
mslatour <sanderlator@gmail.com>
2013-07-18 22:56:28 +0200

    Fixed typo

--- neo4jinit.sh
+++ neo4jinit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # create 'roots' index
-curl -s -X POST -H 'Content-Type: application/json' -d '{"name": "roots"}' $N4J_URL/db/data/index/node/
+curl -s -X POST -H 'Content-Type: application/json' -d '{"name": "roots"}' $NEO4J_URL/db/data/index/node/
 # create 'users' index
-curl -s -X POST -H 'Content-Type: application/json' -d '{"name": "users"}' $N4J_URL/db/data/index/node/
+curl -s -X POST -H 'Content-Type: application/json' -d '{"name": "users"}' $NEO4J_URL/db/data/index/node/
 
 # create idea root node and add to roots index
-idIdeaRootJson=`curl -s -X POST -H 'Content-Type: application/json' -d '{"query": "create (n {} ) return ID(n)"}' $N4J_URL/db/data/cypher`
+idIdeaRootJson=`curl -s -X POST -H 'Content-Type: application/json' -d '{"query": "create (n {} ) return ID(n)"}' $NEO4J_URL/db/data/cypher`
 echo $idIdeaRootJson
 idIdeaRoot=`python -c "raw=$idIdeaRootJson;print raw['data'][0][0]"`
 
-curl -X POST -H 'Content-Type: application/json' -d "{\"key\": \"root\", \"value\": \"idea\", \"uri\": \"node/$idIdeaRoot\"}" $N4J_URL/db/data/index/node/roots
+curl -X POST -H 'Content-Type: application/json' -d "{\"key\": \"root\", \"value\": \"idea\", \"uri\": \"node/$idIdeaRoot\"}" $NEO4J_URL/db/data/index/node/roots
 
 # create user root node and add to roots index
-idUserRootJson=`curl -s -X POST -H 'Content-Type: application/json' -d '{"query": "create (n {} ) return ID(n)"}' $N4J_URL/db/data/cypher`
+idUserRootJson=`curl -s -X POST -H 'Content-Type: application/json' -d '{"query": "create (n {} ) return ID(n)"}' $NEO4J_URL/db/data/cypher`
 idUserRoot=`python -c "raw=$idUserRootJson;print raw['data'][0][0]"`
 
-curl -s -X POST -H 'Content-Type: application/json' -d "{\"key\": \"root\", \"value\": \"user\", \"uri\": \"node/$idUserRoot\"}" $N4J_URL/db/data/index/node/roots
+curl -s -X POST -H 'Content-Type: application/json' -d "{\"key\": \"root\", \"value\": \"user\", \"uri\": \"node/$idUserRoot\"}" $NEO4J_URL/db/data/index/node/roots
